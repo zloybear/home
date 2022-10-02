c9860b79a78dc20ef1f5d8b5460b319dab0364a0
avarangePyLover <pypipeeequev1@mail.ru>
2022-10-02 14:15:30 +1000

    1

---
+++ bot cheriot.py
@@ -0,0 +1,22 @@
+import telebot
+token = ('не твой а наш')
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет")
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="Кнопка":
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Кнопка 2")
+        markup.add(item1)
+        bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+    elif message.text=="Кнопка 2":
+        bot.send_message(message.chat.id,'Спасибо за прочтение статьи!')
+bot.infinity_polling()
